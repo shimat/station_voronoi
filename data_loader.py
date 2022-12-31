ac126ffcd7a6b5254d7b989612d106cdec98617c
shimat <schimatk@gmail.com>
2023-01-01 06:30:36 +0900

    all

--- data_loader.py
+++ data_loader.py
@@ -4,10 +4,11 @@ import json
 import numpy as np
 import numpy.typing as npt
 import streamlit as st
-from typing import Iterable
+from typing import Any, Iterable
 from pathlib import Path
 from pprint import pprint
 from pyproj import Transformer
+import shapely.geometry
 
 
 def get_station_locations(file_name: str) -> npt.NDArray[np.float64]:
@@ -35,12 +36,57 @@ def get_pref_contour(pref_name: str) -> npt.NDArray[np.float64]:
 
     coordinates = list(itertools.chain.from_iterable(feature["geometry"]["coordinates"]))
     longest = sorted(coordinates, key=lambda c: len(c))[-1]
-    #pprint(longest)
     return np.array(longest)
 
 
 def get_pref_contour_transformed(pref_name: str, transformer: Transformer)-> npt.NDArray[np.float64]:
     return np.array([transformer.transform(lat, lon)[::-1] for lon, lat in get_pref_contour(pref_name)])
+
+
+def get_island_contour(name: str) -> npt.NDArray[np.float64]:
+    with open("geojson/pref.geojson", encoding="utf-8-sig") as file:
+        gj = json.load(file)
+
+    shapes = _collect_island_shapes(name, gj)
+    merged_shape = next(shapes)
+    for s in shapes:
+        merged_shape = merged_shape.union(s)
+
+    coordinates = list(list(poly.exterior.coords) for poly in merged_shape.geoms)
+    longest = sorted(coordinates, key=lambda c: len(c))[-1]
+
+    return np.array(longest)
+
+
+def get_main_islands_contours() -> npt.NDArray[np.float64]:
+    shapes = (
+        get_island_contour("北海道"),
+        get_island_contour("本州"),
+        get_island_contour("四国"),
+        get_island_contour("九州"))    
+    return np.vstack(shapes)
+
+
+def get_main_islands_contours_transformed(transformer: Transformer)-> npt.NDArray[np.float64]:
+    return np.array([transformer.transform(lat, lon)[::-1] for lon, lat in get_main_islands_contours()])
+
+def _collect_island_shapes(name: str, gj: dict[str, Any]) -> Iterable[shapely.geometry.shape]:
+    def get_indices(name: str) -> list[int]:
+        match name:
+            case "北海道":
+                return [0]
+            case "本州":
+                return list(range(1, 35))
+            case "四国":
+                return list(range(35, 39))
+            case "九州":
+                return list(range(39, 46))
+            case "沖縄本島":
+                return [46]
+            case _:
+                raise ValueError(f"Not supported island name: {name}")
+
+    return (shapely.geometry.shape(gj['features'][i]['geometry']) for i in get_indices(name))
 
 
 def _first_true(iterable, pred):
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy.typing as npt
 from collections import deque
 from typing import Any, Iterable
 from sklearn.preprocessing import MinMaxScaler
-from data_loader import get_all_station_locations_transformed, get_pref_contour_transformed
+from data_loader import get_all_station_locations_transformed, get_pref_contour_transformed, get_island_contour, get_main_islands_contours_transformed
 from transformers import get_transformer
 
 IMAGE_SIZE = 2000
@@ -23,7 +23,7 @@ def show_plot(data: Iterable[tuple[Any, Any]]) -> None:
     st.pyplot(fig)
 
 
-def voronoi(points: npt.NDArray[np.float64], pref_contour: npt.NDArray[np.float64]) -> None:
+def voronoi(points: npt.NDArray[np.float64], island_contours: npt.NDArray[np.float64]) -> None:
     subdiv = cv2.Subdiv2D((0, 0, IMAGE_SIZE+1, IMAGE_SIZE+1))
     for p in points:
         subdiv.insert((p[0], p[1]))
@@ -37,7 +37,7 @@ def voronoi(points: npt.NDArray[np.float64], pref_contour: npt.NDArray[np.float6
     #print(facets)
     cv2.polylines(img, [(f+50).astype(int) for f in facets], True, (0, 255, 255), thickness=2)
 
-    cv2.polylines(img, [np.array([(f+50).astype(int) for f in pref_contour])], True, (255, 255, 255), thickness=4)
+    cv2.polylines(img, [np.array([(f+50).astype(int) for f in island_contours])], True, (255, 255, 255), thickness=4)
     #img_mask = np.zeros_like(img)
     #cv2.fillPoly(img_mask, [np.array([(f+50).astype(int) for f in pref_contour])], (255, 255, 255))
     #img = cv2.bitwise_and(img, img_mask)
@@ -47,40 +47,55 @@ def voronoi(points: npt.NDArray[np.float64], pref_contour: npt.NDArray[np.float6
     st.image(img, channels="BGR", caption="Voronoi")
     #st.image(img_mask, channels="BGR", caption="Mask")
 
-transformer = get_transformer("北海道", "")
-scaler = MinMaxScaler((0, IMAGE_SIZE))
 
-pref_contour = get_pref_contour_transformed("北海道", transformer)
-station_locations = get_all_station_locations_transformed("csv", transformer)
-#show_plot([
-#    (pref_contour[:, 0], pref_contour[:, 1]),
-#    (station_locations[:, 0], station_locations[:, 1])])
+def distance_transform(points: npt.NDArray[np.float64], island_contours: npt.NDArray[np.float64]) -> None:
+    img = np.full((IMAGE_SIZE+100, IMAGE_SIZE+100, 1), 255, dtype=np.uint8)
+    for p in station_locations:
+        cv2.rectangle(img, (p+50).astype(int), (p+51).astype(int), (0, 0, 0), thickness=2)
+    dist = cv2.distanceTransform(img, cv2.DIST_L2, 5)
 
-combi = np.vstack((pref_contour, station_locations))
-combi_norm = scaler.fit_transform(combi)
-pref_contour, station_locations = np.vsplit(combi_norm, [len(pref_contour)])
-show_plot([
-    (pref_contour[:, 0], pref_contour[:, 1]),
-    (station_locations[:, 0], station_locations[:, 1])])
+    mask = np.zeros_like(dist, dtype=np.uint8)
+    cv2.fillPoly(mask, [np.array([(f+50).astype(int) for f in island_contours])], (255, 255, 255))
+    dist = dist * (mask / 255)
 
-voronoi(station_locations, pref_contour)
+    _, maxVal, _, maxLoc = cv2.minMaxLoc(dist)
+
+    cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
+    dist_u8 = cv2.applyColorMap((dist*255).astype(np.uint8), cv2.COLORMAP_HOT)
+
+    cv2.drawMarker(dist_u8, maxLoc, (0, 0, 255), markerSize=30, thickness=5)
+
+    dist_u8 = cv2.flip(dist_u8, 0)
+    st.image(dist_u8, channels="BGR", caption="distance")
 
 
-img = np.full((IMAGE_SIZE+100, IMAGE_SIZE+100, 1), 255, dtype=np.uint8)
-for p in station_locations:
-    cv2.rectangle(img, (p+50).astype(int), (p+51).astype(int), (0, 0, 0), thickness=2)
-dist = cv2.distanceTransform(img, cv2.DIST_L2, 5)
+tab1, tab2 = st.tabs(("北海道", "全国"))
 
-mask = np.zeros_like(dist, dtype=np.uint8)
-cv2.fillPoly(mask, [np.array([(f+50).astype(int) for f in pref_contour])], (255, 255, 255))
-dist = dist * (mask / 255)
+with tab1:
+    transformer = get_transformer("北海道", "")
+    scaler = MinMaxScaler((0, IMAGE_SIZE))
 
-_, maxVal, _, maxLoc = cv2.minMaxLoc(dist)
+    island_contours = get_pref_contour_transformed("北海道", transformer)
+    station_locations = get_all_station_locations_transformed("csv", transformer)
+    #show_plot([
+    #    (pref_contour[:, 0], pref_contour[:, 1]),
+    #    (station_locations[:, 0], station_locations[:, 1])])
 
-cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
-dist_u8 = cv2.applyColorMap((dist*255).astype(np.uint8), cv2.COLORMAP_HOT)
+    combi = np.vstack((island_contours, station_locations))
+    combi_norm = scaler.fit_transform(combi)
+    island_contours, station_locations = np.vsplit(combi_norm, [len(island_contours)])
+    show_plot([
+        (island_contours[:, 0], island_contours[:, 1]),
+        (station_locations[:, 0], station_locations[:, 1])])
 
-cv2.drawMarker(dist_u8, maxLoc, (0, 0, 255), markerSize=30, thickness=5)
+    voronoi(station_locations, island_contours)
 
-dist_u8 = cv2.flip(dist_u8, 0)
-st.image(dist_u8, channels="BGR", caption="distance")
+    distance_transform(station_locations, island_contours)
+
+
+with tab2:
+    transformer = get_transformer("東京都", "")
+
+    island_contours = get_main_islands_contours_transformed(transformer)
+    show_plot([
+        (island_contours[:, 0], island_contours[:, 1]),])
